88bd94c9d012e6a0859e793d403cf1c74e1e5459
mbake94 <mbake94@gmail.com>
2020-04-16 06:58:21 -0700

    first commit

---
+++ src/SmallestandLargest.java
@@ -0,0 +1,39 @@
+/*
+Prompt you for 3 numbers
+if number1 > number2 && number3
+print number1 is Greatest
+else if, if number2 > number1 && number3
+print number 2 is greatest
+
+ */
+import java.util.Scanner;
+
+public class SmallestandLargest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input the 1st number: ");
+        int num1 = in.nextInt();
+
+        System.out.print("Input the 2nd number: ");
+        int num2 = in.nextInt();
+
+        System.out.print("Input the 3rd number: ");
+        int num3 = in.nextInt();
+
+
+        if ((num1 > num2) && (num1 > num3)) {
+            System.out.println("The greatest: " + num1);
+        } else if ((num2 > num1) && (num2 > num3)) {
+            System.out.println("The greatest: " + num2);
+        } else if ((num3 > num1) && (num3 > num2)) {
+            System.out.println("The greatest: " + num3);
+        } if ((num1 < num2) && (num1 < num3)) {
+            System.out.println("The smallest: " + num1);
+        } else if ((num2 < num1) && (num2 < num3)) {
+            System.out.println("The smallest: " + num2);
+        } else if ((num3 < num1) && (num3 < num1)) {
+            System.out.println("The smallest: " + num3);
+        }
+    }
+}
